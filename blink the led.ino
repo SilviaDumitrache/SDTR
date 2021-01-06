2984d816003e05c5b72de0bb8140054ba411c573
SilviaDumitrache <silvia_d97@yahoo.com>
2021-01-06 23:13:43 +0200

    created task for led blink

--- blink the led.ino
+++ blink the led.ino
@@ -1,15 +1,27 @@
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
 #define LED 13 //the led in connected to digital pin 13
 
 void setup() {
-  
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT); //digital pin set as output
+
+  xTaskCreate (red, "RED", 100, NULL, 1, NULL);
+  vTaskStartScheduler();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH); //led on
-  delay(1000);             //wait 1s
-  digitalWrite(LED, LOW);  //led off
-  delay(1000);
+  // idle task
+
+}
+
+void red(void *pvParameters)
+{
+  while(1)
+  {
+    digitalWrite(LED, HIGH); //led on
+    delay(1000);             //wait 1s
+    digitalWrite(LED, LOW);  //led off
+    delay(1000);
+  }
 }
