a3df216131510788e9f612f446fc6622a501b98f
SilviaDumitrache <silvia_d97@yahoo.com>
2021-01-09 12:30:20 +0200

    added & used PulseSensorPlayground library

--- two_tasks.ino
+++ two_tasks.ino
@@ -1,9 +1,28 @@
 #include <Arduino_FreeRTOS.h>
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
+/*---------------------- Variables ---------------------*/
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+
+/*---------------------- Tasks handlers---------------------*/
+TaskHandle_t taskDeletedHandle;
+TaskHandle_t TaskBlink_Handler;
+TaskHandle_t TaskSerial_Handler;
+
+/*---------------------- Tasks definition---------------------*/
 //define two tasks: blink & analog read
 void TaskBlink(void *pvParameters);
 void TaskAnalogRead(void *pvParameters);
+//void TaskDelete(void *pvParameters);
 
+
+/*---------------------- Setup ---------------------*/
 //setup for reset/power on the board
  void setup() {
   // put your setup code here, to run once:
@@ -13,6 +32,17 @@ void TaskAnalogRead(void *pvParameters);
     ;//wait for serial port to connect
   }
 
+//Configuration of the PulseSensor (variables are assigned)
+  pulseSensor.analogInput(PulseWire);   
+//  pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);     
+
+//Check if the pulse sensor object was created. If yes => we see a signal. 
+   if (pulseSensor.begin()) {
+    Serial.println("PulseSensor Object successfuly created!");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
+
+
 //setup tasks to run independently
   xTaskCreate( 
                 TaskBlink,      //task function
@@ -20,7 +50,7 @@ void TaskAnalogRead(void *pvParameters);
                 128,            //stack size (words)
                 NULL,           //parameter -> input of the task
                 2,              //priority of task
-                NULL);          //task handle (reference the task from ouside the execution
+                &TaskBlink_Handler);          //task handle (reference the task from ouside the execution
               
 
 
@@ -30,15 +60,17 @@ void TaskAnalogRead(void *pvParameters);
                 128,                 //stack size (words)
                 NULL,                //parameter -> input of the task
                 1,                   //priority of task
-                NULL);               //task handle (reference the task from ouside the execution
+                &TaskSerial_Handler);               //task handle (reference the task from ouside the execution
+              
               
 }
 
-//The task scheduler is automatically started
 
 void loop() {
   //is empty => things are done in tasks
 }
+
+//The task scheduler is automatically started
 
 
 
@@ -51,9 +83,9 @@ void TaskBlink( void *pvParameters) {
   pinMode(LED, OUTPUT);
   for(;;) //never return or exit
   {
-    digitalWrite(LED,HIGH);                 //led on
+    digitalWrite(LED,HIGH);                  //led on
     vTaskDelay(1000/portTICK_PERIOD_MS);    //wait 1sec
-    digitalWrite(LED,LOW);                  //led off
+    digitalWrite(LED,LOW);                   //led off
     vTaskDelay(1000/portTICK_PERIOD_MS);    //wait 1sec
   }
 }
@@ -65,11 +97,22 @@ void TaskAnalogRead(void *pvParameters) {
   for (;;)
   {
     // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
+    int sensorValue = pulseSensor.getBeatsPerMinute(); //sensorValue holds the BPM value
 
-    // print out the value you read:
-    Serial.println(sensorValue);
+    if (pulseSensor.sawStartOfBeat()){
+      Serial.println("♥ HeartBeat found ! ");  //if test = true => print message
+      Serial.println("BPM:");
+      Serial.println(sensorValue);              //print out the value read
+    }
+
 
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
+
+
+
+
+
+
+
