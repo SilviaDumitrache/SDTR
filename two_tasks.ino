cd5afda812f973f3795b51c243eff3992ddd78fb
SilviaDumitrache <silvia_d97@yahoo.com>
2021-01-09 19:37:44 +0200

    implementation of semaphore

--- two_tasks.ino
+++ two_tasks.ino
@@ -1,6 +1,7 @@
-#include <Arduino_FreeRTOS.h>
+#include <Arduino_FreeRTOS.h>          //Freertos library
 #define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#include <semphr.h>                    //add the FreeRTOS functions for semaphores (flags)
 
 /*---------------------- Variables ---------------------*/
 const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
@@ -9,11 +10,13 @@ int Threshold = 550;           // Determine which Signal to "count as a beat" an
 
 PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
 
+//declaration of a mutex semaphore handle
+SemaphoreHandle_t xSerialSemaphore;   //used to ensure that only one task is accessing the resource at any time
 
 /*---------------------- Tasks handlers---------------------*/
-TaskHandle_t taskDeletedHandle;
-TaskHandle_t TaskBlink_Handler;
-TaskHandle_t TaskSerial_Handler;
+//TaskHandle_t taskDeletedHandle;
+//TaskHandle_t TaskBlink_Handler;
+//TaskHandle_t TaskSerial_Handler;
 
 /*---------------------- Tasks definition---------------------*/
 //define two tasks: blink & analog read
@@ -30,6 +33,13 @@ void TaskAnalogRead(void *pvParameters);
   Serial.begin(9600); //serial communication initialized at 9600 bits per seconds
   while(!Serial) {
     ;//wait for serial port to connect
+  }
+
+  if (xSerialSemaphore == NULL)                   //check that the serial semaphore has not already been created
+  {
+    xSerialSemaphore = xSemaphoreCreateMutex();   //create a mutex semaphore, used to manage serial port
+    if ((xSerialSemaphore) != NULL)
+      xSemaphoreGive ((xSerialSemaphore));        //serial port available to use
   }
 
 //Configuration of the PulseSensor (variables are assigned)
@@ -50,7 +60,7 @@ void TaskAnalogRead(void *pvParameters);
                 128,            //stack size (words)
                 NULL,           //parameter -> input of the task
                 2,              //priority of task
-                &TaskBlink_Handler);          //task handle (reference the task from ouside the execution
+                NULL);          //task handle (reference the task from ouside the execution
               
 
 
@@ -60,17 +70,17 @@ void TaskAnalogRead(void *pvParameters);
                 128,                 //stack size (words)
                 NULL,                //parameter -> input of the task
                 1,                   //priority of task
-                &TaskSerial_Handler);               //task handle (reference the task from ouside the execution
+                NULL);               //task handle (reference the task from ouside the execution
               
-              
+
+
+              //The task scheduler is automatically started
 }
 
 
 void loop() {
   //is empty => things are done in tasks
 }
-
-//The task scheduler is automatically started
 
 
 
@@ -81,12 +91,20 @@ void TaskBlink( void *pvParameters) {
   //initialize led on pin 13
   #define LED 13 //the led in connected to digital pin 13
   pinMode(LED, OUTPUT);
+
   for(;;) //never return or exit
   {
+    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
+    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE ){
+      Serial.println("Led blinking"); }
+    
     digitalWrite(LED,HIGH);                  //led on
-    vTaskDelay(1000/portTICK_PERIOD_MS);    //wait 1sec
+    vTaskDelay(1000/portTICK_PERIOD_MS);     //wait 1sec
     digitalWrite(LED,LOW);                   //led off
-    vTaskDelay(1000/portTICK_PERIOD_MS);    //wait 1sec
+    vTaskDelay(1000/portTICK_PERIOD_MS);     //wait 1sec
+
+    xSemaphoreGive(xSerialSemaphore);       //give serial port to others
+    vTaskDelay(1);    // one tick delay (15ms) in between reads for stability
   }
 }
 
@@ -99,14 +117,17 @@ void TaskAnalogRead(void *pvParameters) {
     // read the input on analog pin 0:
     int sensorValue = pulseSensor.getBeatsPerMinute(); //sensorValue holds the BPM value
 
+    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE ) {
+
     if (pulseSensor.sawStartOfBeat()){
       Serial.println("♥ HeartBeat found ! ");  //if test = true => print message
       Serial.println("BPM:");
       Serial.println(sensorValue);              //print out the value read
-    }
 
-
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+      xSemaphoreGive(xSerialSemaphore);       //give serial port to others
+      vTaskDelay(1);    // one tick delay (15ms) in between reads for stability
+    }}
+//    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
 
